6c96f06f2a740140b63299862d79b80231f555af
Agrate0718 <zionagrate@gmail.com>
2023-01-30 08:41:39 -0500

    downloaded and tested tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TailwindTest from "./tailwindTest";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <div className="">
+        <Routes>
+          <Route path="/" element={<TailwindTest />} />
+        </Routes>
+
+        {/* <header className="text-center">
+        <TailwindTest />
       </header>
-    </div>
+      <div className=" text-center pt-60">
+        <div className="text-7xl">Zapatoes</div>
+        <div className="text-2xl pt-8 pb-20">A shoe selling site</div>
+      </div> */}
+      </div>
+    </Router>
   );
 }
 
